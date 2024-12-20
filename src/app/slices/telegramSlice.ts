7f53ccf8cd181d96805c004627b8b1f3d616964e
VinaySingh1045 <vy0891547@gmail.com>
2024-12-20 12:00:25 +0530

    update the telegramSlice with extraReducers

--- src/app/slices/telegramSlice.ts
+++ src/app/slices/telegramSlice.ts
@@ -1,20 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchTelegram } from "../actions/telegramActions";
 
 const telegramSlice = createSlice({
     name: "telegramAuth",
     initialState: {
+        loading: false,
         telegramUser: null,
+        isError: false,
     },
+
     reducers: {
-        setTelegramUser: (state, action) => {
-            state.telegramUser = action.payload;
-        },
         clearTelegramData: (state) => {
             state.telegramUser = null;
         },
     },
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchTelegram.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(fetchTelegram.fulfilled, (state, action) => {
+            state.loading = false;
+            state.telegramUser = action.payload;
+        })
+        builder.addCase(fetchTelegram.rejected, (state) => {
+            state.isError = true;
+        })
+    }
+
 })
 
-export const { setTelegramUser, clearTelegramData } = telegramSlice.actions;
+export const { clearTelegramData } = telegramSlice.actions;
 
 export default telegramSlice.reducer;
