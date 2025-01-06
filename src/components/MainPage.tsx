3a56aa2c7ad49ba19fac9ed73dfae943aca0f799
Bhavesh-Teli <telibhavesh11@gmail.com>
2025-01-06 18:24:18 +0530

    implemented  telegram session validation

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,101 +1,110 @@
-import { Col, Row, Spin } from "antd"
-import "../index.css"
-import Timelog from "./Timelog"
-import TodoCard from "./TodoCard"
-import { useEffect, useState } from "react"
-import { useNavigate, useSearchParams } from "react-router-dom"
-import { useDispatch } from "react-redux"
-import { setUser } from "../app/slices/authSlice"
-import { Verify } from "../services/authAPI"
-import { AppDispatch } from "../app/store"
-import { UpdateTodoInProgressDate } from "../services/todoAPI"
-import { fetchTodos } from "../app/actions/todosAction"
-import axios from "axios"
-import { API_END_POINT } from "../utils/constants"
-import { fetchTelegram } from "../app/actions/telegramActions"
+import { Col, Row, Spin } from "antd";
+import "../index.css";
+import Timelog from "./Timelog";
+import TodoCard from "./TodoCard";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "../app/slices/authSlice";
+import { Verify } from "../services/authAPI";
+import { AppDispatch } from "../app/store";
+import { UpdateTodoInProgressDate } from "../services/todoAPI";
+import { fetchTodos } from "../app/actions/todosAction";
+import axios from "axios";
+import { API_END_POINT } from "../utils/constants";
+import { fetchTelegram } from "../app/actions/telegramActions";
+import { TelegramSessionValidation } from "../services/telegramAPI";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const [loading, setLoading] = useState(false);
+  const [searchParams] = useSearchParams();
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch<AppDispatch>();
-    const [loading, setLoading] = useState(false);
-    const [searchParams] = useSearchParams();
+  useEffect(() => {
+    const telegramSessionCheck = async () => {
+      try {
+        await TelegramSessionValidation();
+        dispatch(fetchTelegram());
+      } catch (error) {
+        console.error("Failed to update todo date:", error);
+      }
+    };
+    telegramSessionCheck();
+  }, []);
+  useEffect(() => {
+    const handleOAuthCallback = async () => {
+      const code = searchParams.get("code");
+      if (!code) {
+        console.error("Authorization code not found.");
+        return;
+      }
 
-    useEffect(() => {
-        const handleOAuthCallback = async () => {
-            const code = searchParams.get("code");
-            if (!code) {
-                console.error("Authorization code not found.");
-                return;
-            }
+      try {
+        await axios.get(`${API_END_POINT}/oauth2callback`, {
+          params: { code },
+          withCredentials: true,
+        });
+        dispatch(fetchTelegram());
+        navigate("/");
+      } catch (error) {
+        console.error("Error during OAuth2 callback:", error);
+      }
+    };
 
-            try {
-                await axios.get(`${API_END_POINT}/oauth2callback`, {
-                    params: { code },
-                    withCredentials: true
-                });
-                dispatch(fetchTelegram())
-                navigate("/");
-            } catch (error) {
-                console.error("Error during OAuth2 callback:", error);
-            }
-        };
+    handleOAuthCallback();
+  }, [searchParams, navigate, dispatch]);
 
-        handleOAuthCallback();
-    }, [searchParams, navigate, dispatch]);
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await Verify();
+        dispatch(setUser(response));
+      } catch (error) {
+        console.error("Verification failed:", error);
+        navigate("/login");
+      }
+    };
+    verifyToken();
+  }, [dispatch, navigate]);
 
+  useEffect(() => {
+    const updateInProgressTodosDate = async () => {
+      try {
+        await UpdateTodoInProgressDate();
+        dispatch(fetchTodos());
+      } catch (error) {
+        console.error("Failed to update todo date:", error);
+      }
+    };
+    updateInProgressTodosDate();
+  }, [dispatch]);
 
-    useEffect(() => {
-        const verifyToken = async () => {
-            try {
-                const response = await Verify();
-                dispatch(setUser(response));
-            } catch (error) {
-                console.error("Verification failed:", error);
-                navigate("/login");
-            }
-        };
-        verifyToken();
-    }, [dispatch, navigate]);
+  return (
+    <>
+      {loading && (
+        <div className="full-page-spin">
+          <Spin size="large" />
+        </div>
+      )}
+      <Row>
+        <Col md={15}>
+          <div
+            style={{
+              padding: "10px",
+              position: "relative",
+              zIndex: "1",
+            }}
+          >
+            <Timelog />
+          </div>
+        </Col>
+        <Col md={9}>
+          <TodoCard setLoading={setLoading} />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-    useEffect(() => {
-        const updateInProgressTodosDate = async () => {
-            try {
-                await UpdateTodoInProgressDate();
-                dispatch(fetchTodos());
-            } catch (error) {
-                console.error("Failed to update todo date:", error);
-            }
-        };
-        updateInProgressTodosDate();
-    }, [dispatch]);
-
-    return (
-        <>
-            {loading && (
-                <div className="full-page-spin">
-                    <Spin size="large" />
-                </div>
-            )}
-            <Row >
-                <Col md={15} >
-                    <div
-                        style={{
-                            padding: "10px",
-                            position: "relative",
-                            zIndex: "1"
-                        }}
-                    >
-                        <Timelog />
-                    </div>
-                </Col>
-                <Col md={9} >
-                    <TodoCard setLoading={setLoading} />
-                </Col>
-            </Row>
-
-        </>
-    )
-}
-
-export default MainPage
+export default MainPage;
--- src/services/telegramAPI.tsx
+++ src/services/telegramAPI.tsx
@@ -1,70 +1,90 @@
 import axios from "axios";
 import { API_END_POINT } from "../utils/constants";
 
-
 export const LoginApiTelegram = async (phone: any) => {
-    const payload = { telegram: { phone } };
-    try {
-        const res = await axios.post(`${API_END_POINT}/login`, payload, { withCredentials: true })
-        return res.data
-    } catch (error) {
-        console.error('Error While Login with Telegram:', error);
-        throw error;
-    }
-}
-
+  const payload = { telegram: { phone } };
+  try {
+    const res = await axios.post(`${API_END_POINT}/login`, payload, {
+      withCredentials: true,
+    });
+    return res.data;
+  } catch (error) {
+    console.error("Error While Login with Telegram:", error);
+    throw error;
+  }
+};
 
 export const SubmitApiTelegram = async (userData) => {
-    const { phone, code } = userData;
-    if (!phone || !code) {
-        throw new Error('Phone number and code are required');
-    }
-    try {
-        const res = await axios.post(`${API_END_POINT}/submit-otp`, userData)
-        return res.data
-    } catch (error) {
-        console.error('Error While Login with Telegram:', error);
-        throw error;
-    }
-}
+  const { phone, code } = userData;
+  if (!phone || !code) {
+    throw new Error("Phone number and code are required");
+  }
+  try {
+    const res = await axios.post(`${API_END_POINT}/submit-otp`, userData);
+    return res.data;
+  } catch (error) {
+    console.error("Error While Login with Telegram:", error);
+    throw error;
+  }
+};
 
 export const LogoutApiTelegram = async (phone: any) => {
-    try {
-        const res = await axios.post(`${API_END_POINT}/logout`, { phone })
-        return res.data
-    } catch (error) {
-        console.error('Error during Telegram logout:', error.response?.data || error.message);
-        throw error;
-    }
-}
-
+  try {
+    const res = await axios.post(`${API_END_POINT}/logout`, { phone });
+    return res.data;
+  } catch (error) {
+    console.error(
+      "Error during Telegram logout:",
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
 
 export const GetTelegram = async () => {
-    try {
-        const res = await axios.get(`${API_END_POINT}/getTelegram`, { withCredentials: true });
-        return res.data;
-    } catch (error) {
-        console.error('Error fetching time logs:', error);
-        throw error;
-    }
-}
+  try {
+    const res = await axios.get(`${API_END_POINT}/getTelegram`, {
+      withCredentials: true,
+    });
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching time logs:", error);
+    throw error;
+  }
+};
 
 export const SendTodosToChat = async (userData) => {
-    try {
-        const response = await axios.post(`${API_END_POINT}/sendTask`, userData);
-        return response.data;
-    } catch (error) {
-        console.error("Error sending todos to chat:", error);
-        throw error;
-    }
-}
+  try {
+    const response = await axios.post(`${API_END_POINT}/sendTask`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Error sending todos to chat:", error);
+    throw error;
+  }
+};
 
 export const SendTodosToGoogleChat = async (userData) => {
-    try {
-        const response = await axios.post(`${API_END_POINT}/send-message`, userData, { withCredentials: true });
-        return response.data;
-    } catch (error) {
-        console.error("Error sending todos to chat:", error);
-        throw error;
-    }
-}
+  try {
+    const response = await axios.post(
+      `${API_END_POINT}/send-message`,
+      userData,
+      { withCredentials: true }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error sending todos to chat:", error);
+    throw error;
+  }
+};
+
+export const TelegramSessionValidation = async () => {
+  try {
+    const response = await axios.get(`${API_END_POINT}/check-Session-id`, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error in validating telegram session:", error);
+    throw error;
+  }
+};
