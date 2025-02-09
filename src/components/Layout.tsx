7ecf22b92732337b20385e1dafdca7cf882d56f8
VinaySingh1045 <vy0891547@gmail.com>
2025-02-09 15:53:14 +0530

    Fix authentication flow

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,31 +4,40 @@ import { Verify } from "../services/authAPI";
 import { useEffect } from "react";
 import { setUser } from "../redux/slices/authSlice";
 import { useDispatch } from "react-redux";
-import { AppDispatch } from "../redux/store";
+import { AppDispatch, RootState } from "../redux/store";
 import Footer from "./shared/Footer";
+import { useSelector } from "react-redux";
 const Layout = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch<AppDispatch>();
-    useEffect(() => {
-        const verifyToken = async () => {
-          try {
-            const response = await Verify();
-            dispatch(setUser(response));
-          } catch (error) {
-            console.error("Verification failed:", error);
-            navigate("/login");
-          }
-        };
-        verifyToken();
-      }, [dispatch, navigate]);
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector((state: RootState) => state.auth);
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await Verify();
+        dispatch(setUser(response));
+      } catch (error) {
+        console.error("Verification failed:", error);
+        navigate("/login");
+      }
+    };
+    verifyToken();
+  }, [dispatch, navigate]);
 
-    return (
-        <div>
-            <Navbar />
-            <Outlet />
-            <Footer/>
-        </div>
-    )
+  useEffect(() => {
+    if (!user?.fullName) {
+      navigate("/login");
+    }
+  }, [user, navigate])
+
+
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
