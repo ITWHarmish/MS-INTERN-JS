c6f44303e53ae19d35740282db608f1b16c3fec8
VinaySingh1045 <vy0891547@gmail.com>
2025-02-11 11:39:18 +0530

    Add font family and height to Navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -129,8 +129,8 @@ const Navbar = () => {
                 <Menu
                     onClick={onMenuClick}
                     selectedKeys={[current]}
+                    className="check"
                     style={{
-                        display: "flex",
                         gap: "1px",
                     }}
                     mode="horizontal"
@@ -142,17 +142,17 @@ const Navbar = () => {
 
                     {
                         telegramUser?.google?.tokens?.access_token ?
-                            <Button disabled type="default">Connect Google</Button>
+                            <Button style={{ fontFamily: "Rubik" }} disabled type="default">Connect Google</Button>
                             :
-                            <Button onClick={googleLogin} type="default">Connect Google</Button>
+                            <Button style={{ fontFamily: "Rubik" }} onClick={googleLogin} type="default">Connect Google</Button>
                     }
                     {telegramUser?.telegram?.session_id ? (
-                        <Button type="default" disabled>
+                        <Button style={{ fontFamily: "Rubik" }} type="default" disabled>
                             Telegram Connected
                         </Button>
                     ) : (
                         <>
-                            <Button onClick={showModal} type="default">
+                            <Button style={{ fontFamily: "Rubik" }} onClick={showModal} type="default">
                                 Connect Telegram
                             </Button>
 
@@ -208,7 +208,7 @@ const Navbar = () => {
                     {user &&
                         <Popover content={popoverContent} trigger="click">
                             <span>
-                                <span style={{ cursor: "pointer", marginRight: "7px" }}>{user.fullName}</span>
+                                <span style={{ cursor: "pointer", marginRight: "7px", fontFamily: "Rubik" }}>{user.fullName}</span>
                                 <Avatar src={telegramUser?.google?.profile?.picture || undefined} style={{ marginRight: "7px", cursor: "pointer" }} icon={!telegramUser?.google?.profile?.picture ? initials : undefined} />
                             </span>
                         </Popover>
