0de24e9e72a3efbf9029a3a39d39bec646566e4f
VinaySingh1045 <vy0891547@gmail.com>
2025-03-06 10:27:53 +0530

    Add a chat bot and change the place of totalHours view

--- src/components/Timelog.tsx
+++ src/components/Timelog.tsx
@@ -3,11 +3,13 @@ import dayjs from 'dayjs'
 import Tasktable from './Tasktable'
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../redux/store';
+import { AppDispatch, RootState } from '../redux/store';
 import { fetchTimelogs } from '../redux/actions/timelogActions';
+import { useSelector } from 'react-redux';
 
 const Timelog = ({ selectedDate, setSelectedDate }) => {
     const dispatch = useDispatch<AppDispatch>();
+    const { timelogs } = useSelector((state: RootState) => state.timelog)
 
     const handleDateChange = (date) => {
         if (date) {
@@ -15,6 +17,11 @@ const Timelog = ({ selectedDate, setSelectedDate }) => {
             setSelectedDate(dayjs(formattedDate));
         }
     }
+
+    const totalHours = timelogs.reduce((total, timelog) => {
+        const hours = typeof timelog?.hours === 'number' ? timelog.hours : 0;
+        return total + hours;
+    }, 0);
 
     useEffect(() => {
         const formattedDate = selectedDate.format("YYYY-MM-DD");
@@ -25,11 +32,16 @@ const Timelog = ({ selectedDate, setSelectedDate }) => {
 
     return (
         <>
-            <Card style={{position: "relative", height:"100%" }} title={"Timelog"} extra={
-                <DatePicker defaultValue={selectedDate} onChange={handleDateChange} />
-            }>
+            <Card style={{ position: "relative", height: "100%" }}
+                title={<div>
+                    <span>Timelog :</span>
+                    <span style={{ fontWeight: "normal", marginLeft:"7px" }}>{`${totalHours.toFixed(2)} hours`}</span>
+                </div>}
+                extra={
+                    <DatePicker defaultValue={selectedDate} onChange={handleDateChange} />
+                }>
                 <Tasktable selectedDate={selectedDate} />
-            </Card>
+            </Card >
         </>
     )
 }
