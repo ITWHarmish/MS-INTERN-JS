664e6b9d0df25bfcbc83473083a3b7a473dc1904
VinaySingh1045 <vy0891547@gmail.com>
2025-03-21 15:21:55 +0530

    Add a loading State

--- src/components/reports/ReportUserDetail.tsx
+++ src/components/reports/ReportUserDetail.tsx
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store";
 import { useDispatch } from "react-redux";
 import { fetchProgressReport } from "../../redux/actions/progressReportActions";
+import Spinner from "../../utils/Spinner";
 
 const ReportUserDetail = () => {
     const { RangePicker } = DatePicker;
@@ -15,49 +16,49 @@ const ReportUserDetail = () => {
     const dispatch = useDispatch<AppDispatch>();
     const navigate = useNavigate();
     const [form] = Form.useForm();
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [currentStep, setCurrentStep] = useState(0);
     const { user } = useSelector((state: RootState) => state.auth)
     const { progressReport } = useSelector((state: RootState) => state.report)
     const { token } = theme.useToken();
 
     useEffect(() => {
-        const fetchProgressReport = async () => {
-            await GetProgressReport(reportId);
-        }
-        fetchProgressReport();
-    }, [reportId])
-
-    useEffect(() => {
         const fetchData = async () => {
-            if (user?.admin) {
-                const res = await GetProgressReport(reportId);
-                if (res) {
-                    form.setFieldsValue({
-                        studentName: res.studentName,
-                        enrollmentNo: res.enrollmentNo,
-                        course: res.course,
-                        division: res.division,
-                        projectTitle: res.projectTitle,
-                        duration: res.duration
-                            ? [dayjs(res.duration.from), dayjs(res.duration.to)]
-                            : [],
-                    });
+            try {
+
+                if (user?.admin) {
+                    const res = await GetProgressReport(reportId);
+                    if (res) {
+                        form.setFieldsValue({
+                            studentName: res.studentName,
+                            enrollmentNo: res.enrollmentNo,
+                            course: res.course,
+                            division: res.division,
+                            projectTitle: res.projectTitle,
+                            duration: res.duration
+                                ? [dayjs(res.duration.from), dayjs(res.duration.to)]
+                                : [],
+                        });
+                    }
+                } else {
+                    const report = progressReport.find(report => report._id === reportId);
+                    if (report) {
+                        form.setFieldsValue({
+                            studentName: report.studentName,
+                            enrollmentNo: report.enrollmentNo,
+                            course: report.course,
+                            division: report.division,
+                            projectTitle: report.projectTitle,
+                            duration: report.duration
+                                ? [dayjs(report.duration.from), dayjs(report.duration.to)]
+                                : [],
+                        });
+                    }
                 }
-            } else {
-                const report = progressReport.find(report => report._id === reportId);
-                if (report) {
-                    form.setFieldsValue({
-                        studentName: report.studentName,
-                        enrollmentNo: report.enrollmentNo,
-                        course: report.course,
-                        division: report.division,
-                        projectTitle: report.projectTitle,
-                        duration: report.duration
-                            ? [dayjs(report.duration.from), dayjs(report.duration.to)]
-                            : [],
-                    });
-                }
+            } catch (error) {
+                console.error("error While Fetching", error);
+            } finally {
+                setLoading(false);
             }
         };
 
@@ -124,86 +125,89 @@ const ReportUserDetail = () => {
 
     return (
         <>
-            <div style={{ backgroundColor: token.colorBgLayout === "White" ? "#f0f2f5" : "#1a1c1f", marginBottom: "50px", overflow: "", height: "100vh" }}>
-                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "20px", flexDirection: "column" }}>
-                    <Steps current={currentStep} style={{ width: "80%", maxWidth: "800px", marginBottom: "20px" }}>
-                        <Step title="Fill Details" />
-                        <Step title="Add Tasks" />
-                        {user?.admin &&
-                            <Step title="Intern Evaluation" />
-                        }
-                        <Step title="Review & Submit" />
-                    </Steps>
-                    <Card
-                        title="Fill Up Your Details"
-                        extra={
-                            <Button onClick={() => form.submit()} loading={loading} type="primary" htmlType="submit">
-                                Next
-                            </Button>
-                        }
-                        style={{ width: "80%", maxWidth: "900px", padding: "24px", marginBottom: "22px" }}
-                    >
-                        <Form
-                            form={form}
-                            onFinish={handleSubmit}
-                            layout="vertical"
-                        >
-                            <Row gutter={24}>
-                                <Col span={12}>
-                                    <Form.Item
-                                        label="Student Name"
-                                        name="studentName"
-                                        rules={[{ required: true, message: "Name is required!" }]}
-                                    >
-                                        <Input placeholder="Enter Full Name " />
-                                    </Form.Item>
-                                    <Form.Item
-                                        label="Enrollment Number"
-                                        name="enrollmentNo"
-                                        rules={
-                                            [{ required: true, message: "Enrollement Number is required!" },
-                                            ]}
-                                    >
-                                        <Input placeholder="SR22BSIT000" />
-                                    </Form.Item>
-                                    <Form.Item
-                                        label="Class/Course"
-                                        name="course"
-                                        rules={[
-                                            { required: true, message: "Please Enter your Course!" },
-                                        ]}
-                                    >
-                                        <Input placeholder="BSC IT" />
-                                    </Form.Item>
-                                </Col>
+            {
+                loading ? <Spinner /> :
+                    <div style={{ backgroundColor: token.colorBgLayout === "White" ? "#f0f2f5" : "#1a1c1f", marginBottom: "50px", overflow: "", height: "100vh" }}>
+                        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "20px", flexDirection: "column" }}>
+                            <Steps current={currentStep} style={{ width: "80%", maxWidth: "800px", marginBottom: "20px" }}>
+                                <Step title="Fill Details" />
+                                <Step title="Add Tasks" />
+                                {user?.admin &&
+                                    <Step title="Intern Evaluation" />
+                                }
+                                <Step title="Review & Submit" />
+                            </Steps>
+                            <Card
+                                title="Fill Up Your Details"
+                                extra={
+                                    <Button onClick={() => form.submit()} loading={loading} type="primary" htmlType="submit">
+                                        Next
+                                    </Button>
+                                }
+                                style={{ width: "80%", maxWidth: "900px", padding: "24px", marginBottom: "22px" }}
+                            >
+                                <Form
+                                    form={form}
+                                    onFinish={handleSubmit}
+                                    layout="vertical"
+                                >
+                                    <Row gutter={24}>
+                                        <Col span={12}>
+                                            <Form.Item
+                                                label="Student Name"
+                                                name="studentName"
+                                                rules={[{ required: true, message: "Name is required!" }]}
+                                            >
+                                                <Input placeholder="Enter Full Name " />
+                                            </Form.Item>
+                                            <Form.Item
+                                                label="Enrollment Number"
+                                                name="enrollmentNo"
+                                                rules={
+                                                    [{ required: true, message: "Enrollement Number is required!" },
+                                                    ]}
+                                            >
+                                                <Input placeholder="SR22BSIT000" />
+                                            </Form.Item>
+                                            <Form.Item
+                                                label="Class/Course"
+                                                name="course"
+                                                rules={[
+                                                    { required: true, message: "Please Enter your Course!" },
+                                                ]}
+                                            >
+                                                <Input placeholder="BSC IT" />
+                                            </Form.Item>
+                                        </Col>
 
-                                <Col span={12}>
+                                        <Col span={12}>
 
-                                    <Form.Item
-                                        label="Division"
-                                        name="division"
-                                    >
-                                        <Input placeholder="Enter your Division " />
-                                    </Form.Item>
-                                    <Form.Item
-                                        label="Project Title"
-                                        name="projectTitle"
-                                    >
-                                        <Input placeholder="Enter your Project Title " />
-                                    </Form.Item>
-                                    <Form.Item
-                                        label="Report Duration"
-                                        name="duration"
-                                        rules={[{ required: true, message: "Please Select your Report date!" }]}
-                                    >
-                                        <RangePicker placeholder={["From", "To"]} style={{ width: "100%" }} />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-                        </Form>
-                    </Card>
-                </div>
-            </div>
+                                            <Form.Item
+                                                label="Division"
+                                                name="division"
+                                            >
+                                                <Input placeholder="Enter your Division " />
+                                            </Form.Item>
+                                            <Form.Item
+                                                label="Project Title"
+                                                name="projectTitle"
+                                            >
+                                                <Input placeholder="Enter your Project Title " />
+                                            </Form.Item>
+                                            <Form.Item
+                                                label="Report Duration"
+                                                name="duration"
+                                                rules={[{ required: true, message: "Please Select your Report date!" }]}
+                                            >
+                                                <RangePicker placeholder={["From", "To"]} style={{ width: "100%" }} />
+                                            </Form.Item>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Card>
+                        </div>
+                    </div>
+            }
         </>
     )
 }
