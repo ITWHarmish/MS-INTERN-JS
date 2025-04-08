840282da9742254b5e7c6537bb9ee69e575e38a9
VinaySingh1045 <vy0891547@gmail.com>
2025-04-08 18:35:51 +0530

    Loader changes

--- src/utils/Spinner.tsx
+++ src/utils/Spinner.tsx
@@ -1,14 +1,14 @@
-import { grid } from 'ldrs'
-grid.register()
+import { dotWave } from 'ldrs'
+dotWave.register()
 
 const Spinner = () => {
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "65vh" }}>
-            <l-grid
-                size="80"
-                speed="1.5"
-                color="#474787"
-            ></l-grid>
+            <l-dot-wave
+                size="60"
+                speed="2"
+                color="#fff"
+            ></l-dot-wave>
         </div>
     )
 }
