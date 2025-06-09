2465bdd38e7a0b644e87d7f27bb0ae6a3863a9bd
Daksh <pateldaksh382@gmail.com>
2025-06-09 17:18:42 +0530

    fix internlist component

--- src/components/admin/AddIntern.tsx
+++ src/components/admin/AddIntern.tsx
@@ -1,158 +1,194 @@
-import { Input, Button, Modal, Form, Row, Col, message, Switch, Select } from "antd";
+import {
+  Input,
+  Button,
+  Modal,
+  Form,
+  Row,
+  Col,
+  message,
+  Switch,
+  Select,
+} from "antd";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../../utils/Spinner";
 import { UserRegister } from "../../services/authAPI";
 import { RootState } from "../../redux/store";
 import { IProfile } from "../../types/ILogin";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const AddIntern = ({ space, visible, onClose, fetchInterns }) => {
+const AddIntern = ({ space, visible, onClose }) => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const queryClient = useQueryClient();
 
-    const [loading, setLoading] = useState(false);
-    const { user } = useSelector((state: RootState) => state.auth)
-    const [isAdmin, setIsAdmin] = useState(false);
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (values: any) => {
+      let payload: IProfile = {
+        fullName: values.fullName,
+        email: values.email,
+        password: values.password,
+      };
 
-    const handleSubmit = async (values) => {
-        let payload: IProfile = {
-            fullName: values.fullName,
-            email: values.email,
-            password: values.password,
+      if (isAdmin) {
+        payload.admin = true;
+      } else {
+        payload = {
+          ...payload,
+          spreadId: values.spreadId,
+          spaceId: values.spaceId,
+          mentorId: user._id,
         };
+      }
 
-        if (isAdmin) {
-            payload.admin = true;
-        } else {
-            payload = {
-                ...payload,
-                spreadId: values.spreadId,
-                spaceId: values.spaceId,
-                mentorId: user._id,
-            };
-        }
-        try {
-            setLoading(true);
-            await UserRegister(payload);
-            fetchInterns();
-            message.success("Intern Added Successfully!");
-        } catch (error) {
-            console.error("Error While Adding Intern: ", error);
-            message.error("Intern failed to add!");
-        } finally {
-            setLoading(false);
-            onClose();
-        }
-    };
+      return await UserRegister(payload);
+    },
+    onSuccess: () => {
+      message.success("Intern Added Successfully!");
+      queryClient.invalidateQueries({ queryKey: ["interns"] });
+      onClose();
+    },
+    onError: (error: any) => {
+      console.error("Error While Adding Intern: ", error);
+      message.error("Intern failed to add!");
+    },
+  });
 
-    return (
-        <Modal title={"Add Intern"} open={visible} onCancel={onClose} footer={null} centered width={1000}>
-            {loading ? <Spinner /> :
-                <div style={{ padding: "20px" }}>
-
-                    <Form
-                        onFinish={handleSubmit}
-                        layout="vertical"
+  return (
+    <Modal
+      title={"Add Intern"}
+      open={visible}
+      onCancel={onClose}
+      footer={null}
+      centered
+      width={1000}
+    >
+      {isPending ? (
+        <Spinner />
+      ) : (
+        <div style={{ padding: "20px" }}>
+          <Form onFinish={mutate} layout="vertical">
+            <Row gutter={24}>
+              <Col span={12}>
+                <Form.Item
+                  label="Full Name"
+                  name="fullName"
+                  rules={[
+                    { required: true, message: "Please Enter Full Name!" },
+                  ]}
+                >
+                  <Input placeholder="Roman Reigns" />
+                </Form.Item>
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    { required: true, message: "Please enter your password!" },
+                  ]}
+                >
+                  <Input.Password placeholder="Password" />
+                </Form.Item>
+                {!isAdmin && (
+                  <Form.Item
+                    label="Spread Id"
+                    name="spreadId"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please Enter the Spread Id!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="1608285224" />
+                  </Form.Item>
+                )}
+                <Form.Item>
+                  <>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "7px",
+                      }}
                     >
-                        <Row gutter={24}>
-                            <Col span={12}>
-                                <Form.Item
-                                    label="Full Name"
-                                    name="fullName"
-                                    rules={[{ required: true, message: "Please Enter Full Name!" }]}
-                                >
-                                    <Input placeholder="Roman Reigns" />
-                                </Form.Item>
-                                <Form.Item
-                                    label="Password"
-                                    name="password"
-                                    rules={[{ required: true, message: "Please enter your password!" }]}
-                                >
-                                    <Input.Password placeholder="Password" />
-                                </Form.Item>
-                                {!isAdmin &&
-                                    <Form.Item
-                                        label="Spread Id"
-                                        name="spreadId"
-                                        rules={[
-                                            { required: true, message: "Please Enter the Spread Id!" },
-                                        ]}
-                                    >
-                                        <Input placeholder="1608285224" />
-                                    </Form.Item>
-                                }
-                                <Form.Item>
-                                    <>
-                                        <div style={{ display: "flex", flexDirection: "column", gap: "7px" }}>
-                                            <label>Admin</label>
-                                            <Switch style={{ width: "20px" }} checked={isAdmin} onChange={setIsAdmin} />
-                                        </div>
-                                    </>
-                                </Form.Item>
-                            </Col>
+                      <label>Admin</label>
+                      <Switch
+                        style={{ width: "20px" }}
+                        checked={isAdmin}
+                        onChange={setIsAdmin}
+                      />
+                    </div>
+                  </>
+                </Form.Item>
+              </Col>
 
-                            <Col span={12}>
+              <Col span={12}>
+                <Form.Item
+                  label="E-Mail"
+                  name="email"
+                  rules={[
+                    { required: true, message: "Please enter your email!" },
+                    { type: "email", message: "Please enter a valid email!" },
+                  ]}
+                >
+                  <Input placeholder="E-Mail" />
+                </Form.Item>
 
-                                <Form.Item
-                                    label="E-Mail"
-                                    name="email"
-                                    rules={[
-                                        { required: true, message: 'Please enter your email!' },
-                                        { type: 'email', message: 'Please enter a valid email!' },
-                                    ]}
-                                >
-                                    <Input
-                                        placeholder="E-Mail"
-                                    />
-                                </Form.Item>
-
-                                <Form.Item
-                                    label="Confirm Password"
-                                    name="confirmPassword"
-                                    dependencies={["password"]}
-                                    rules={[
-                                        { required: true, message: "Please confirm your password!" },
-                                        ({ getFieldValue }) => ({
-                                            validator(_, value) {
-                                                if (!value || getFieldValue("password") === value) {
-                                                    return Promise.resolve();
-                                                }
-                                                return Promise.reject(new Error("Passwords do not match!"));
-                                            },
-                                        })
-                                    ]}
-                                >
-                                    <Input.Password placeholder="Confirm Password" />
-                                </Form.Item>
-                                {!isAdmin &&
-                                    <Form.Item
-                                        label="Space Id"
-                                        name="spaceId"
-                                        rules={[
-                                            { required: true, message: "Please Enter the Space Id!" },
-                                        ]}
-                                    >
-                                        <Select showSearch placeholder="Select a space">
-                                            {space && space.map(item => (
-                                                <Select.Option key={item.id} value={item.id}>
-                                                    {item.name}
-                                                </Select.Option>
-                                            ))}
-                                        </Select>
-                                    </Form.Item>
-                                }
-                            </Col>
-                        </Row>
-                        <Form.Item>
-                            <div>
-                                <Button type="primary" htmlType="submit">
-                                    Submit
-                                </Button>
-                            </div>
-                        </Form.Item>
-                    </Form>
-                </div>
-            }
-        </Modal >
-    );
+                <Form.Item
+                  label="Confirm Password"
+                  name="confirmPassword"
+                  dependencies={["password"]}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please confirm your password!",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        if (!value || getFieldValue("password") === value) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(
+                          new Error("Passwords do not match!")
+                        );
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password placeholder="Confirm Password" />
+                </Form.Item>
+                {!isAdmin && (
+                  <Form.Item
+                    label="Space Id"
+                    name="spaceId"
+                    rules={[
+                      { required: true, message: "Please Enter the Space Id!" },
+                    ]}
+                  >
+                    <Select showSearch placeholder="Select a space">
+                      {space &&
+                        space.map((item) => (
+                          <Select.Option key={item.id} value={item.id}>
+                            {item.name}
+                          </Select.Option>
+                        ))}
+                    </Select>
+                  </Form.Item>
+                )}
+              </Col>
+            </Row>
+            <Form.Item>
+              <div>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </div>
+            </Form.Item>
+          </Form>
+        </div>
+      )}
+    </Modal>
+  );
 };
 
 export default AddIntern;
--- src/components/admin/InternList.tsx
+++ src/components/admin/InternList.tsx
@@ -1,150 +1,129 @@
-import { Table, Card, Button, message } from "antd";
-import type { TableProps } from 'antd';
+import { Table, Card, Button, Switch } from "antd";
+import type { TableProps } from "antd";
 import { IColumnsReports } from "../../types/IReport";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../../redux/store";
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import { GetInternsByMentorId, GetSpaceId } from "../../services/adminAPI";
+import { useState } from "react";
 import AddIntern from "./AddIntern";
+import { getInternsHook, spaceListHook } from "../../Hooks/internListhook";
 
 const InternList = () => {
-    const navigate = useNavigate();
-    const { user } = useSelector((state: RootState) => state.auth)
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [students, setStudents] = useState<{ _id: string; fullName: string }[]>([]);
-    const [space, setSpace] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const fetchInterns = async () => {
-        if (!user || !user._id) {
-            console.warn("User or Mentor ID is missing, skipping API call");
-            return;
-        }
-        setLoading(true);
-        try {
-            const res = await GetInternsByMentorId(user._id);
-            setStudents(res.data || []);
-        } catch (error) {
-            console.error("Error Intern List:", error);
-            message.error("Failed to fetch Intern List.");
-        } finally {
-            setLoading(false);
-        }
-    };
+  const { data: space = [], isLoading: spaceLoading } = spaceListHook(user);
 
-    useEffect(() => {
-        fetchInterns();
-        fetchSpaceId();
-    }, [user]);
+  const { data: students = [], isLoading: internsLoading } =
+    getInternsHook(user);
 
-    const fetchSpaceId = async () => {
-        if (user && user.admin) {
-            setLoading(true);
-            try {
-                const res = await GetSpaceId();
-                const spaces = res.data?.filter(item => item.name);
-                setSpace(spaces)
-            } catch (error) {
-                console.error("Error fetching space ID:", error);
-            } finally {
-                setLoading(false);
-            }
-        }
-    }
+  const studentsWithKeys = students.map((student) => ({
+    ...student,
+    key: student._id,
+  }));
+  const handleFileClick = (id: string) => {
+    navigate(`/profile/${id}`);
+  };
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const columns: TableProps<IColumnsReports>['columns'] = [
-        {
-            title: 'Sr. No.',
-            dataIndex: 'srNo',
-            key: 'srNo',
-            align: 'center',
-            render: (_, __, index) => index + 1,
-            width: "80px"
-        },
-        {
-            title: 'Full Name',
-            dataIndex: 'fullName',
-            key: 'fullName',
-            render: (fullName, record: { _id: string }) => (
-                <a
-                    onClick={() => handleFileClick(record._id)}
-                    style={{ color: "inherit", cursor: 'pointer', textDecoration: 'underline' }}
-                >
-                    {`${fullName}`}
-                </a>
-            )
-        },
-        {
-            title: 'Email',
-            dataIndex: 'email',
-            key: 'email',
-            align: 'center',
-        },
-    ];
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
-    const handleFileClick = (id: string) => {
-        navigate(`/profile/${id}`);
-    };
+  const columns: TableProps<IColumnsReports>["columns"] = [
+    {
+      title: "Sr. No.",
+      dataIndex: "srNo",
+      key: "srNo",
+      align: "center",
+      render: (_, __, index) => index + 1,
+      width: "80px",
+    },
+    {
+      title: "Full Name",
+      dataIndex: "fullName",
+      key: "fullName",
+      render: (fullName, record: { _id: string }) => (
+        <a
+          onClick={() => handleFileClick(record._id)}
+          style={{
+            color: "inherit",
+            cursor: "pointer",
+            textDecoration: "underline",
+          }}
+        >
+          {fullName}
+        </a>
+      ),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      align: "center",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+      align: "center",
+      render: (_, record: { _id: string }) => <Switch />,
+    },
+  ];
 
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
+  return (
+    <>
+      <div style={{ padding: "20px", height: "84vh" }}>
+        <Card
+          className="internlist"
+          style={{ position: "relative", height: "77vh" }}
+          title={"Interns List"}
+          extra={
+            <>
+              <Button
+                style={{ marginLeft: "15px" }}
+                onClick={showModal}
+                type="primary"
+              >
+                Add Intern
+              </Button>
+              <AddIntern
+                space={space}
+                visible={isModalOpen}
+                onClose={handleCancel}
+              />
+            </>
+          }
+        >
+          <div style={{ paddingTop: "10px" }}>
+            <Table<IColumnsReports>
+              columns={columns}
+              dataSource={studentsWithKeys}
+              pagination={false}
+              bordered
+              size="small"
+              loading={internsLoading || spaceLoading}
+              sticky={true}
+              className="ScrollInProgress"
+              style={{
+                height: "calc(65vh - 88px)",
+                position: "absolute",
+                overflowY: "auto",
+                overflowX: "hidden",
+                left: "10px",
+                right: "0",
+                paddingRight: "10px",
+              }}
+            />
+          </div>
+        </Card>
+      </div>
+    </>
+  );
+};
 
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
-
-    return (
-        <>
-            <div style={{ padding: "20px", height: "88vh" }}>
-
-                <Card style={{ position: "relative", height: "65vh" }}
-                    title={
-                        "Interns List"
-                    }
-                    extra={
-                        <>
-                            <Button
-                                style={{ marginLeft: "15px" }}
-                                onClick={showModal}
-                                type="primary"
-                            >
-                                Add Intern
-                            </Button>
-                            <AddIntern space={space} visible={isModalOpen} onClose={handleCancel} fetchInterns={fetchInterns} />
-
-                        </>
-                    }
-                >
-                    <div
-                        style={{ paddingTop: "10px" }}
-                    >
-                        <Table<IColumnsReports>
-                            columns={columns}
-                            dataSource={students}
-                            pagination={false}
-                            bordered
-                            size="small"
-                            loading={loading}
-                            sticky={true}
-                            className="ScrollInProgress"
-                            style={{
-                                height: "calc(65vh - 88px)",
-                                position: "absolute",
-                                overflowY: "auto",
-                                overflowX: "hidden",
-                                left: "10px",
-                                right: "0",
-                                paddingRight: "10px",
-                            }}
-                        />
-                    </div>
-                </Card >
-            </div>
-        </>
-    )
-}
-
-export default InternList
+export default InternList;
